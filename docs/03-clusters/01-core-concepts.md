79d1703ddafb85b38486e37ff33b8494f9c3efec
Binura Gunasekara <binura.g@gmail.com>
2020-10-23 09:34:31 +1100

    Audit-fixed dependencies

---
+++ docs/03-clusters/01-core-concepts.md
@@ -0,0 +1,6 @@
+---
+id: 01-core-concepts
+title: Platformer and Kubernetes - Core concepts
+sidebar_label: Core concepts
+---
+
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,13 @@
 module.exports = {
     sidebar: {
         'Getting Started': ['getting-started/01-introduction'],
+        Introduction: [],
+        Administration: [],
+        'Kubernetes Clusters': ['03-clusters/01-core-concepts'],
+        Environments: [],
+        Applications: [],
+        Integrations: [],
+        Tutorials: [],
         Guides: ['styleguide', '01-organizations', '02-projects'],
         'API Reference': ['api-reference/cli'],
     },
