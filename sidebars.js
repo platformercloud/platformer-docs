451749f380e72bd363a729b9c7762a4cdf90bc88
Binura Gunasekara <binura.g@gmail.com>
2020-10-26 13:00:32 +1100

    remove pxe doc assets

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,14 @@
 module.exports = {
-    sidebar: {
-        'Getting Started': ['getting-started/01-introduction'],
-        Introduction: [],
-        Administration: [],
-        'Kubernetes Clusters': ['03-clusters/01-core-concepts'],
-        Environments: [],
-        Applications: [],
-        Integrations: [],
-        Tutorials: [],
-        Guides: ['styleguide', '01-organizations', '02-projects'],
-        'API Reference': ['api-reference/cli'],
+    docs: {
+        'Getting Started': 'getting-started/01-introduction',
+        Introduction: ['styleguide'],
+        'Administration and Access Management': ['styleguide'],
+        'Connecting Kubernetes Clusters': ['03-clusters/01-core-concepts'],
+        'Managing Environments': ['styleguide'],
+        'Deploying Applications': ['styleguide'],
+        Integrations: ['styleguide'],
+        // Tutorials: ['styleguide'],
+        // Guides: ['styleguide', '01-organizations', '02-projects'],
+        // 'API Reference': ['api-reference/cli'],
     },
 };
